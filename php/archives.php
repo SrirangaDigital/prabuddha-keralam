39f57b60d81d3de8ecbb2288b742766e87febc7e
Arjuna <arjun.kashyap@yahoo.co.in>
2015-01-12 21:59:30 +0530

    Commit No. 22 - Description of archive icons made consistent with taht of home page

--- php/archives.php
+++ php/archives.php
@@ -15,24 +15,24 @@
 						<section>
 							<a href="volumes.php">	<span class="icon major fa-book accent2" style="cursor:pointer;"></span>
 							<h3>Volumes</h3>
-							<p>Lists all the volumes which are in Prabuddha Keralam.</p></a>
+							<p>List of all volumes published till date</p></a>
 						</section>
 						<section>
 							<a href="articles.php">	<span class="icon major fa-pencil accent3" style="cursor:pointer;"></span>
 							<h3>Articles</h3>
-							<p>Lists all the articles which are in Prabuddha Keralam.</p></a>
+							<p>List of all articles sorted alphabetically</p></a>
 						</section>
 					</div>
 					<div class="features-row">
 						<section>
 							<a href="authors.php"><span class="icon major fa-user accent4" style="cursor:pointer;"></span>
 							<h3>Author</h3>
-							<p>Lists all the authors who have contributed for Prabuddha Keralam.</p></a>
+							<p>List of all authors in alphabetic order</p></a>
 						</section>
 						<section>
 							<a href="search.php"><span class="icon major fa-search accent5" style="cursor:pointer;"></span>
 							<h3>Search</h3>
-							<p>Search in the entire volumes of Prabuddha Keralam.</p></a>
+							<p>Search through the entire collection</p></a>
 						</section>
 					</div>
 				</section>
