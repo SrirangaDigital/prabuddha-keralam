ea6b184e05a4c0cce2cb21c070dcf2f12763858d
Shruthisdst <shruthitr.nayak@gmail.com>
2016-08-02 17:15:21 +0530

    No. 59 - For rk math link added in footer

--- php/footer.php
+++ php/footer.php
@@ -4,8 +4,8 @@
 					<li><a href="https://www.facebook.com/pages/Prabuddha-Keralam/528116447198404" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 				</ul>
 				<ul class="copyright">
-					<li style="width: 50%">&copy; Sri Ramakrishna Math, Thrissur, Kerala.<br />All rights reserved.</li>
-					<li style="width: 50%">Digitized by<br /><a href="http://www.srirangadigital.com/">Sriranga Digital Software Technologies Private Limited</a></li>
+					<li style="width: 50%">&copy; Sri Ramakrishna Math, Thrissur, Kerala.<br />All rights reserved.<br /><a href="http://www.rkmthrissur.org/" target="_blank">www.rkmthrissur.org</a></li>
+					<li style="width: 50%">Digitized by<br /><a href="http://www.srirangadigital.com/" target="_blank">Sriranga Digital Software Technologies Private Limited</a></li>
 				</ul>
 			</footer>
 		</body>
