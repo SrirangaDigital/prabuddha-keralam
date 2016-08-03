1f280cd45c0eca0f2bf9875cd2c8f6b05bd37c14
shivasdst <shivably04sdst@gmail.com>
2016-08-03 15:58:46 +0530

    No. 68 - php moved to .junk folder

--- php/sec_nav.php
+++
@@ -1,8 +0,0 @@
-				<div id="sec_nav">
-					<ul class="actions">
-						<li><a href="volumes.php" class="button vol_bag icon fa-book">Volumes</a></li>
-						<li><a href="articles.php" class="button art_bag icon fa-pencil">Articles</a></li>
-						<li><a href="authors.php" class="button aut_bag icon fa-user">Authors</a></li>
-						<li><a href="search.php" class="button sea_bag icon fa-search">Search</a></li>
-					</ul>
-				</div>
